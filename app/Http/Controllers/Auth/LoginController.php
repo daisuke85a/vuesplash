0b3b79c7cdc1f92d9c35b91d14a1a2dafc49d93f
daisuke <dsaito85a@gmail.com>
2019-08-29 23:24:41 +0900

    ログイン成功

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request; // ★ 追加
 
 class LoginController extends Controller
 {
@@ -36,4 +37,10 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    // ★ メソッド追加
+    protected function authenticated(Request $request, $user)
+    {
+        return $user;
+    }
+
 }
